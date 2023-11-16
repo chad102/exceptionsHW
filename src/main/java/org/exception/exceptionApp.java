c7b9741112fae10cb627bfcb14e9fa0bfc6e162e
Чингис <cianeed08@gmail.com>
2023-11-16 08:32:26 +0300

    create new Maven project create dependencies + import libs

---
+++ src/main/java/org/exception/exceptionApp.java
@@ -0,0 +1,10 @@
+package org.exception;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class exceptionApp {
+    public static void main(String[] args) {
+        SpringApplication.run(exceptionApp.class, args);
+    }
+}
